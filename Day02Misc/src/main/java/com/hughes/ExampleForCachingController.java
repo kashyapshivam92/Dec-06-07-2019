73b4a6bdd9fb89df2dc7617886627016195c6334
prabhu-durasoft <prabhu.bits@gmail.com>
2019-12-07 15:03:59 +0530

    Day02

--- Day02Misc/src/main/java/com/hughes/ExampleForCachingController.java
+++ Day02Misc/src/main/java/com/hughes/ExampleForCachingController.java
@@ -1,6 +1,9 @@
 package com.hughes;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.Cache.ValueWrapper;
+import org.springframework.cache.CacheManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ExampleForCachingController {
 	@Autowired private MyService myService;
+
+	@Autowired
+	private CacheManager cacheManager;
 	
 	@GetMapping("/info/{name}")
 	public String getInfo(@PathVariable String name) {
+		Cache cache =  cacheManager.getCache("message");
+		ValueWrapper vw = cache.get(name);
+		
+		if(vw != null) {
+			System.out.println("---Present in the cache " + vw.get());
+		}
 		return myService.getMessage(name);
 	}
 }
+
+
+
+
+
+
+
+
--- Day02Misc/src/main/java/com/hughes/MyService.java
+++ Day02Misc/src/main/java/com/hughes/MyService.java
@@ -1,10 +1,12 @@
 package com.hughes;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyService {
 
+	@Cacheable(value = "message")
 	public String getMessage(String name) {
 		System.out.println("**** Getting the message for " + name);
 		try {
